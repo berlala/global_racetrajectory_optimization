c1793ca3c90fcf0f1ba4f1d9e195a60ed012d9c9
Bolin ZHAO <zhaobolin@meituan.com>
2020-09-07 20:52:22 +0800

    add comments

--- helper_funcs_glob/src/result_plots.py
+++ helper_funcs_glob/src/result_plots.py
@@ -49,14 +49,14 @@ def result_plots(plot_opts: dict,
 
         # plot track including optimized path
         plt.figure()
-        plt.plot(refline[:, 0], refline[:, 1], "k--", linewidth=0.7)
-        plt.plot(veh_bound1_virt[:, 0], veh_bound1_virt[:, 1], "b", linewidth=0.5)
-        plt.plot(veh_bound2_virt[:, 0], veh_bound2_virt[:, 1], "b", linewidth=0.5)
-        plt.plot(veh_bound1_real[:, 0], veh_bound1_real[:, 1], "c", linewidth=0.5)
-        plt.plot(veh_bound2_real[:, 0], veh_bound2_real[:, 1], "c", linewidth=0.5)
-        plt.plot(bound1_interp[:, 0], bound1_interp[:, 1], "k-", linewidth=0.7)
-        plt.plot(bound2_interp[:, 0], bound2_interp[:, 1], "k-", linewidth=0.7)
-        plt.plot(trajectory[:, 1], trajectory[:, 2], "r-", linewidth=0.7)
+        plt.plot(refline[:, 0], refline[:, 1], "k--", linewidth=0.7)  # reference center line of track
+        plt.plot(veh_bound1_virt[:, 0], veh_bound1_virt[:, 1], "b", linewidth=0.5) # vehicle safety bound left
+        plt.plot(veh_bound2_virt[:, 0], veh_bound2_virt[:, 1], "b", linewidth=0.5) # vehicle safety bound right
+        plt.plot(veh_bound1_real[:, 0], veh_bound1_real[:, 1], "c", linewidth=0.5) # vehicle physical bound left
+        plt.plot(veh_bound2_real[:, 0], veh_bound2_real[:, 1], "c", linewidth=0.5) # vehicle physical bound right
+        plt.plot(bound1_interp[:, 0], bound1_interp[:, 1], "k-", linewidth = 1.5)  # track bound left
+        plt.plot(bound2_interp[:, 0], bound2_interp[:, 1], "k-", linewidth = 1.5) # track bound right
+        plt.plot(trajectory[:, 1], trajectory[:, 2], "r-", linewidth=1.5)
 
         if plot_opts["imported_bounds"] and bound1_imp is not None and bound2_imp is not None:
             plt.plot(bound1_imp[:, 0], bound1_imp[:, 1], "y-", linewidth=0.7)
