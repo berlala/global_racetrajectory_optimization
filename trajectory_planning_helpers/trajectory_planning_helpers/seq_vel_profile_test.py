636619e6f896cd7772b83884da7f1cff03970c9d
bolin_mac <bolinzhao@yahoo.com>
2021-02-05 15:17:53 +0800

    trick fix dscc method velocity connection issue

--- trajectory_planning_helpers/trajectory_planning_helpers/seq_vel_profile_test.py
+++ trajectory_planning_helpers/trajectory_planning_helpers/seq_vel_profile_test.py
@@ -4,10 +4,11 @@ import math
 import matplotlib.pyplot as  plt
 
 
-
+# Required Input: kappa, el_lengths
 data = np.load('../../kanel_cl.npz') # 读取上赛道闭环轨迹结果
 kappa = data['kappa']
 el_lengths = data['el_lengths']
+data_length = len(kappa)
 
 mu = 1.0 # constant mu
 grav = 9.8
@@ -35,14 +36,23 @@ vel_kappa = []
 for i in kappa:
     vel_kappa.append(min(v_max, math.sqrt(mu*grav/abs(i))))
 
-plt.plot(vel_kappa)
+# temporary fix connection issue
+if vel_kappa[5] == v_max and vel_kappa[-5]==v_max:
+    vel_kappa[:5] = (v_max,)
+    vel_kappa[-5:] = (v_max,)
+
+# Double the track for avoid connection issue
+vel_kappa_double = np.concatenate((vel_kappa,vel_kappa),axis = 0)
+el_lengths_double = np.concatenate((el_lengths,el_lengths),axis = 0)
+
+plt.plot(vel_kappa_double)
 
 # second process: acceleration limit
 vel_acc =[]
-vel_acc.append(vel_kappa[0])
+vel_acc.append(vel_kappa_double[0])
 
-for i in range(len(vel_kappa)-1):
-    vel_acc.append(min(vel_kappa[i+1], math.sqrt(pow(vel_acc[i],2) + 2*_fcn_acc(vel_acc[i])*el_lengths[i])))
+for i in range(len(vel_kappa_double)-1):
+    vel_acc.append(min(vel_kappa_double[i+1], math.sqrt(pow(vel_acc[i],2) + 2*_fcn_acc(vel_acc[i])*el_lengths_double[i])))
 
 plt.plot(vel_acc)
 
@@ -50,9 +60,19 @@ plt.plot(vel_acc)
 vel_dec = vel_acc
 
 for i in range(len(vel_acc)-1,1,-1):
-    vel_dec[i-1] = min(vel_acc[i-1], math.sqrt(max(0.0, pow(vel_dec[i],2) + 2*_fcn_del(vel_dec[i])*el_lengths[i])) )
+    vel_dec[i-1] = min(vel_acc[i-1], math.sqrt(max(0.0, pow(vel_dec[i],2) + 2*_fcn_del(vel_dec[i])*el_lengths_double[i])) )
 
 plt.plot(vel_dec)
-plt.legend(['Curv Cons','Acc cons','Dec cons'])
+
+# final process, rebuild
+index_1 = int(data_length/2)
+patch_1 = vel_dec[index_1:data_length]
+patch_2 = vel_dec[data_length:data_length+index_1]
+vel_rebuild = np.concatenate((patch_2,patch_1),axis=0)
+if len(patch_1) + len(patch_2) != data_length:
+    raise ValueError("length is not matched!")
+
+plt.plot(vel_rebuild)
+plt.legend(['Curv Cons','Acc cons','Dec cons','Final Res'])
 plt.ylabel('Spd[m/s]')
 plt.show()
