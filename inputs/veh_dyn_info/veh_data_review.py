8aa60959780d97c66244beae3912553ae8deb743
bolin.zhao <bolin.zhao@jiduauto.com>
2023-09-22 19:16:29 +0800

    add 3D ggv plot

--- inputs/veh_dyn_info/veh_data_review.py
+++ inputs/veh_dyn_info/veh_data_review.py
@@ -10,8 +10,38 @@ import csv
 # -------------------------------------------------------
 # 对高性能轮胎和赛道，附着系数可以大于1。
 
+# Generation friction cycle based on current velocity and max ax and ay
+def get_current_v_ggv_limit(ax_limit, ay_limit, cur_v):
+    current_ax_limit = np.array([])
+    current_ay_limit = np.array([])
+    ylist = []
+    for i  in range(int(ax_limit+1)): # this will be processed later
+        ylist.append(math.sqrt(1-(i*i)/(ax_limit*ax_limit))*ay_limit) 
+
+    ax_1q = np.array(range(int(ax_limit+1)))
+    ay_1q = np.array(ylist)
+    current_ax_limit = np.append(current_ax_limit,ax_1q)
+    current_ay_limit = np.append(current_ay_limit,ay_1q)
+    ax_2q = np.flipud(ax_1q)
+    ay_2q = np.flipud(-1.0*ay_1q)
+    current_ax_limit = np.append(current_ax_limit,ax_2q[1:])
+    current_ay_limit = np.append(current_ay_limit,ay_2q[1:])
+    ax_3q = -1.0*ax_1q
+    ay_3q = -1.0*ay_1q
+    current_ax_limit = np.append(current_ax_limit,ax_3q[1:])
+    current_ay_limit = np.append(current_ay_limit,ay_3q[1:])
+    ax_4q = np.flipud(-1.0*ax_1q)
+    ay_4q = np.flipud(ay_1q)
+    current_ax_limit = np.append(current_ax_limit,ax_4q[1:])
+    current_ay_limit = np.append(current_ay_limit,ay_4q[1:])
+
+    current_v = np.ones(len(current_ay_limit))*cur_v
+
+    return current_ax_limit, current_ay_limit, current_v
+
+
 # GGV Data
-csv_file_ggv = csv.reader(open('ggv.csv'))
+csv_file_ggv = csv.reader(open('ggv_normal.csv'))
 csv_file_motor = csv.reader(open('ax_max_machines.csv'))
 
 v_ggv = [] # m/s
@@ -44,7 +74,23 @@ plt.ylabel('Acc[m/s^2]')
 plt.show()
 
 fig2 = plt.figure()
-plt.plot(ax_ggv_max,ay_ggv_max,'o')
-plt.xlabel('Acc X [m/s]')
-plt.ylabel('Acc Y [m/s^2]')
+# data preparation
+ax_limit = np.array([])
+ay_limit = np.array([])
+index_v = np.array([])
+
+for i in range(len(v_ggv)):
+    current_limit  = []
+    [cur_ax,cur_ay, cur_v] = get_current_v_ggv_limit(ax_ggv_max[i], ay_ggv_max[i], v_ggv[i])
+    index_v = np.append(index_v,cur_v)
+    ax_limit = np.append(ax_limit,cur_ax)
+    ay_limit = np.append(ay_limit,cur_ay)
+
+ax = fig2.add_subplot(111, projection='3d')
+ax.scatter(ax_limit, ay_limit, index_v)  # plot_surface requires a same size x,y
+plt.title("GGV under Different Speed")
+ax.set_xlabel('X-Acc[m/s]')
+ax.set_ylabel('Y-Acc[m/s^2]')
+ax.set_zlabel('Spd[m/s]')
+# scatter  or plot_trisurf
 plt.show()
